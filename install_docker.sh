c7c57241f839d9ff0198cda999531bb0202383e3
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2023-11-17 16:21:52 -0100

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Instalar o Docker no Rocky Linux Linux
+# By dpnpinnto.github.io
+
+# Verificar se existem atualizações
+sudo dnf check-update
+
+# adiconar o repositório do Dockeer
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# Instalar o software do docker e containers docker-ce (versão comunidade) docker-ce-cli (software de atuação cliente) container.io (plataforma de container utilizada pelo docker)
+sudo dnf install docker-ce docker-ce-cli containerd.io
+
+# Arrancar com o daemon do docker
+sudo systemctl start docker
+
+# Mostrar o estado do serviço arrancado
+sudo systemctl status docker
+
+# Ativar o serviço no arranque do sistema operativo
+sudo systemctl enable docker
