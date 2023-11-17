9a6c6144751fb6fb4d54dcbc4ee840248fd2193f
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2023-11-17 16:46:23 -0100

    Update install_docker_compose.sh

--- install_docker_compose.sh
+++ install_docker_compose.sh
@@ -8,9 +8,11 @@
 # Verificar atualizações do Rocky
 sudo dnf check-update
 
-#  Adicionar o repositório do docker necessário
+# Adicionar o repositório do docker se o necessário pois se foi executa o script de instalar o Docker este repositório já foi colocado
 sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-#
+# Adicionar o docker-compose-plugin 
+sudo dnf install docker-compose-plugin
 
-#
+# Mostrar a versão do Composer
+docker compose version
