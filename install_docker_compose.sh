126107f58024457d665307aa9965a5014081ada7
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2023-11-17 16:43:51 -0100

    Update install_docker_compose.sh

--- install_docker_compose.sh
+++ install_docker_compose.sh
@@ -1,1 +1,16 @@
+#!/usr/bin/env bash
+#
+# Author : Duarte Pinto
+# Date: November 2022
+# Version 1.0.0: "Install Docker Compose"
+#
 
+# Verificar atualizações do Rocky
+sudo dnf check-update
+
+#  Adicionar o repositório do docker necessário
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+#
+
+#
