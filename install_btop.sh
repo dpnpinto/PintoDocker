99ac4eb2050565bd838aebd386ee56c3c4a9b0ed
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2023-11-17 22:36:44 -0100

    Update install_btop.sh

--- install_btop.sh
+++ install_btop.sh
@@ -1,1 +1,15 @@
+#!/usr/bin/env bash
+#
+# Author : Duarte Pinto
+# Date: November 2022
+# Version 1.0.0: "Install Portainer"
+#
 
+#Instalar o repositorio Epel
+sudo dnf -y install epel-release
+
+#Gerar a Cache
+sudo dnf makecache
+
+#Instalar o btop
+sudo dnf install btop
